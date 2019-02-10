015130894d57fe52579a4a9cc5ecf878d65a7bb2
Yannick Thibos <yannick.thibos@gmail.com>
2019-02-10 21:07:36 +0100

    Initial project setup
    * Added Idea folder and target folders to gitignore
    * Root pom created with basic testing dependencies
    * First dummy test

---
+++ src/test/java/be/thibos/personalaccountant/model/ExpenseTest.java
@@ -0,0 +1,13 @@
+package be.thibos.personalaccountant.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class ExpenseTest {
+
+	@Test
+	void dummyTest() {
+		assertThat(true).isTrue();
+	}
+}
