4dcf78e099fcab5857f2dba0fef93d81ddc6aa8a
Yannick Thibos <yannick.thibos@gmail.com>
2019-06-26 05:25:20 +0200

    CDI introduction into the project
    * Why does maven/surefire always need everything to be open for my tests??

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module PersonalAccountant.renameMe {
 	opens be.thibos.personalaccountant.model.entities;
 	opens be.thibos.personalaccountant.model.persistance;
 	opens be.thibos.personalaccountant.controllers;
+	opens be.thibos.personalaccountant.cdi;
 
 	requires org.apache.commons.lang3;
 	requires cdi.api;
